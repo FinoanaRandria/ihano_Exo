341c1661719efc4f695f1eece2e7ebca4866d58d
FinoanaRandria <fitiavanarex4@gmail.com>
2023-02-17 20:34:09 +0300

    Quiz3

---  Exercice Express Questionaire/controllers/ctrl.js
+++  Exercice Express Questionaire/controllers/ctrl.js
@@ -2,9 +2,61 @@ exports.home = (req, res) => {
   res.render("home");
 };
 
-
+exports.quiz = (req,res)=>{
+    res.render('quiz1')
+}
 /* post */
 
 exports.post1 = (req,res)=>{
-    
+
+
+       if(req.body.question1 == "finoana" || req.body.question1 == "landry" || req.body.question1 == "tolotra" || req.body.question1 == "jose" || req.body.question1 == "tohy" || req.body.question1 == "johana" ){
+            res.render('quiz2')
+       }else{
+        res.render('quiz1')
+       }
+
+       console.log(req.body.question1)
+       res.end()
+}
+
+exports.post2 = (req,res)=>{
+
+
+  if(req.body.question2 == "masisou" || req.body.question1 == "dubai" || req.body.question1 == "sql" ){
+       res.render('quiz3')
+  }else{
+   res.render('quiz2')
+  }
+
+  console.log(req.body.question1)
+  res.end()
+}
+
+
+exports.post3 = (req,res)=>{
+
+
+  if(req.body.question3 == "eliot" || req.body.question1 == "angela" || req.body.question1 == "whiterose" ){
+       res.render('quiz4')
+  }else{
+   res.render('quiz3')
+  }
+
+  console.log(req.body.question1)
+  res.end()
+}
+
+
+exports.post4 = (req,res)=>{
+
+
+  if(req.body.question4 == "Masisou" || req.body.question1 == "Dubai" || req.body.question1 == "Sql" ){
+       res.render('quiz5')
+  }else{
+   res.render('quiz4')
+  }
+
+  console.log(req.body.question1)
+  res.end()
 }
---  Exercice Express Questionaire/routes.js
+++  Exercice Express Questionaire/routes.js
@@ -5,15 +5,16 @@ const {post1,post2,post3,post4} = require('./controllers/ctrl')
 const router = Router()
 
 
+
 router.get('/',home)
 
-
+router.get('/Quest',quiz)
 
 
 
 /* partie post */
 
 router.post('/qest1',post1)
-
+router.post('/qest2',post2)
 
 module.exports = router
